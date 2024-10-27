43e22292f6980e0126741d638d96cba95a5eb76d
Christos Nikolaou <me@christosnikolaou.org>
2024-10-27 09:11:03 +0000

    Move styles in component's `<style scoped>`

--- src/icping_frontend/src/App.vue
+++ src/icping_frontend/src/App.vue
@@ -3,21 +3,23 @@ import { ref, onMounted, watch, onBeforeUnmount } from 'vue';
 import { icping_backend } from 'declarations/icping_backend/index';
 
 const url = ref('');
+const uptime = ref([]);
 let intervalId;
 
 onMounted(async () => {
   await icping_backend.getFrequency().then((response) => {
     interval.value = parseInt(response);
-    console.log('FETCH', interval.value);
+  });
+
+  await icping_backend.getUrl().then((response) => {
+    url.value = response;
   });
 });
 
 watch(url, () => {
   if (url !== '') {
     intervalId = setInterval(async () => {
-      console.log('FETCH');
-      const uptime = await icping_backend.getUptime();
-      console.log(uptime);
+      uptime.value = await icping_backend.getUptime();
     }, interval.value * 1000);
   } else {
     stopUptimeReq();
@@ -37,13 +39,10 @@ onBeforeUnmount(() => {
 const interval = ref(1000);
 
 async function handleSubmit(e) {
-  console.log(e);
   e.preventDefault();
   const target = e.target;
   const urlValue = target.querySelector('#url').value;
-  console.log('URL:', urlValue);
   await icping_backend.setUrl(urlValue).then((response) => {
-    console.log(response);
     url.value = response;
   });
   const host = url.value.split('//')[1].split('/')[0];
@@ -54,14 +53,60 @@ async function handleSubmit(e) {
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
+    <h1>ICPing: On-chain healthchecker</h1>
     <br />
 
     <br />
     <form action="#" @submit="handleSubmit">
       <label for="url">Enter the url:</label>
-      <input id="url" alt="Name" type="text" />
+      <input id="url" alt="Name" type="text" :value="url" />
       <button type="submit">Select endpoint</button>
     </form>
     <section>url: {{ url }}</section>
   </main>
 </template>
+
+<style scoped>
+body {
+  font-family: sans-serif;
+  font-size: 1.5rem;
+}
+
+h1 {
+  display: block;
+  margin: auto;
+}
+
+img {
+  max-width: 50vw;
+  max-height: 25vw;
+  display: block;
+  margin: auto;
+}
+
+form {
+  display: flex;
+  justify-content: center;
+  gap: 0.5em;
+  flex-flow: row wrap;
+  max-width: 40vw;
+  margin: auto;
+  align-items: baseline;
+}
+
+button[type='submit'] {
+  padding: 5px 20px;
+  margin: 10px auto;
+  float: right;
+}
+
+#greeting {
+  margin: 10px auto;
+  padding: 10px 60px;
+  border: 1px solid #222;
+}
+
+#greeting:empty {
+  display: none;
+}
+</style>
